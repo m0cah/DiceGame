ab63fc6c9333f64e7691cbbae834ffa5b9765aa2
m0cah <101680882+m0cah@users.noreply.github.com>
2022-04-14 23:16:20 -0400

    Added DiceGame.js

---
+++ DiceGame.js
@@ -0,0 +1,12 @@
+/*
+    Game which simulates rolling a pair of dice!
+
+    @author: Michael Neal
+    @version: 04.14.2022
+
+*/
+
+let diceOne = Math.floor(Math.random()*6 + 1)
+let diceTwo = Math.floor(Math.random()*6 + 1)
+
+console.log("You Rolled: " + diceOne + " and " + diceTwo + "!")
